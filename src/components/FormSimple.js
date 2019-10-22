91747ecaafff325bda57cee789d4c4c2243a05d7
maeky <max.ehle@gmail.com>
2019-10-22 12:16:28 +0200

    Small changes to form

--- src/components/FormSimple.js
+++ src/components/FormSimple.js
@@ -5,15 +5,15 @@ import './Form.css'
 export default ({
   name = 'Simple Form',
   subject = '', // optional subject of the notification email
-  action = ''
+  action = '#'
 }) => (
   <form
     className='Form'
     name={name}
     action={action}
-    method="POST"
+    method='POST'
     data-netlify='true'
-    data-netlify-honeypot='_gotcha'
+    data-netlify-honeypot='bot-field'
   >
     <label className='Form--Label Form--InputText'>
       <input
@@ -57,7 +57,8 @@ export default ({
         required
       />
     </label>
-    <input type='text' name='_gotcha' style={{ display: 'none' }} />
+    <input type="hidden" name="bot-field" />
+    {/* <input type='text' name='_gotcha' style={{ display: 'none' }} /> */}
     {!!subject && <input type='hidden' name='subject' value={subject} />}
     <input type='hidden' name='form-name' value={name} />
     <input
